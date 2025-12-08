51a390144eafe2dd777e60764b26380f519077c3
James Dicker <ajcardsstore@gmail.com>
2025-12-08 05:55:19 +0000

    Fix missing API route reference

--- data/incentive-extras.ts
+++ data/incentive-extras.ts
@@ -4,8 +4,15 @@ export const incentiveExtras: Record<string, number> = {
   // username: extra points (graduations, bonuses, manual rewards)
 
   lewmxy: 150,
-  huxtables1: 3500,
+  huxtables1: 4750,
   "alfie.harnett": 150,
 
   // add more below
 };
+
+
+
+
+// git git add .
+git commit -m "Fix missing API route reference"
+git push
