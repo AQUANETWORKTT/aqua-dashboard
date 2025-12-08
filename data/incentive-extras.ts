ea47d7f0f382c5f05660fd374b0222de086bfab7
James Dicker <ajcardsstore@gmail.com>
2025-12-08 05:59:39 +0000

    Fix missing API route reference

--- data/incentive-extras.ts
+++ data/incentive-extras.ts
@@ -4,7 +4,7 @@ export const incentiveExtras: Record<string, number> = {
   // username: extra points (graduations, bonuses, manual rewards)
 
   lewmxy: 150,
-  huxtables1: 3500,
+  huxtables1: 4750,
   "alfie.harnett": 150,
 
   // add more below
