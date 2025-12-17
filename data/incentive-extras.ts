b87e802928f52ebacb3c4abb9276ab2ed0f5cc63
James Dicker <ajcardsstore@gmail.com>
2025-12-17 10:27:19 +0000

    wow

--- data/incentive-extras.ts
+++ data/incentive-extras.ts
@@ -4,8 +4,9 @@ export const incentiveExtras: Record<string, number> = {
   // username: extra points (graduations, bonuses, manual rewards)
 
   lewmxy: 150,
-  huxtables1: 4750,
+  huxtables1: 6000,
   "alfie.harnett": 1400,
+  k29693k: 8000,
 
   // add more below
 };
