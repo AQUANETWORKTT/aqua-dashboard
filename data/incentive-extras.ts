a4b2799b6cdf2ff4b1ccf03b0a4d0606d007ad66
James Dicker <ajcardsstore@gmail.com>
2025-12-24 20:34:24 +0000

    wow

--- data/incentive-extras.ts
+++ data/incentive-extras.ts
@@ -9,7 +9,7 @@ export const incentiveExtras: Record<string, number> = {
   k29693k: 8000,
   g_streamz1: 150,
   ben_welshdj_:100,
-  1sclassified:2000,
+  "1sclassified":2000,
 
   // add more below
 };
