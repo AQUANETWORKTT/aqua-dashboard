5ee549b45fe2234db03f64068d130b4e35e77120
James Dicker <ajcardsstore@gmail.com>
2026-04-21 20:38:52 +0100

    wow

--- lib/submissions-supabase.ts
+++ lib/submissions-supabase.ts
@@ -1,6 +1,14 @@
 import { createClient } from "@supabase/supabase-js";
 
-export const submissionsSupabase = createClient(
-  process.env.NEXT_PUBLIC_SUBMISSIONS_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUBMISSIONS_SUPABASE_ANON_KEY!
-);
+const url = process.env.NEXT_PUBLIC_SUBMISSIONS_SUPABASE_URL;
+const key = process.env.NEXT_PUBLIC_SUBMISSIONS_SUPABASE_ANON_KEY;
+
+if (!url) {
+  throw new Error("Missing NEXT_PUBLIC_SUBMISSIONS_SUPABASE_URL");
+}
+
+if (!key) {
+  throw new Error("Missing NEXT_PUBLIC_SUBMISSIONS_SUPABASE_ANON_KEY");
+}
+
+export const submissionsSupabase = createClient(url, key);
